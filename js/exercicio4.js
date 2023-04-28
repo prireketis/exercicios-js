89f5de556031b45ce291ecbc335df94ef94287a8
prireketis <prisandrade30@gmail.com>
2023-04-28 12:16:33 -0300

    exercicios js botao imput e alerta

---
+++ js/exercicio4.js
@@ -0,0 +1,4 @@
+document.getElementById("btn-exibir-nome").addEventListener("click", function() {
+    const nomeCompleto = document.getElementById('nome-completo').value;
+    alert('O Nome Completo é: ' + nomeCompleto );	
+  });
